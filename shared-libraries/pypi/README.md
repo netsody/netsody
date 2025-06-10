975784140e4e3e3a9d79870b436ae824249dc56f
Kevin R <dev@roebert.eu>
2025-06-10 16:58:30 +0200

    Python bindings

---
+++ shared-libraries/pypi/README.md
@@ -0,0 +1,43 @@
+[<img src="https://docs.drasyl.org/img/logo-text.svg" alt="drasyl" width="200"/>](https://drasyl.org)
+
+[Website](https://drasyl.org) |
+[Documentation](https://docs.drasyl.org) |
+[Contributing](CONTRIBUTING.md) |
+[Changelog](CHANGELOG.md)
+
+[![Test](https://github.com/drasyl/drasyl/actions/workflows/test.yml/badge.svg)](https://github.com/drasyl/drasyl/actions/workflows/test.yml)
+[![MIT License](https://img.shields.io/badge/license-MIT-blue)](https://opensource.org/licenses/MIT)
+[![Maven Central](https://img.shields.io/maven-central/v/org.drasyl/drasyl-core.svg)](https://mvnrepository.com/artifact/org.drasyl/drasyl-core)
+[![Homebrew](https://img.shields.io/badge/dynamic/json.svg?url=https://raw.githubusercontent.com/drasyl/homebrew-tap/main/Info/drasyl.json&prefix=v&query=$.versions.stable&label=homebrew&color=important)](https://github.com/drasyl/homebrew-tap)
+[![Chocolatey](https://img.shields.io/chocolatey/v/drasyl)](https://chocolatey.org/packages/drasyl)
+[![Python Package Index](https://img.shields.io/pypi/v/drasyl.svg)](https://pypi.org/project/drasyl)
+[![Docker Pulls](https://img.shields.io/docker/pulls/drasyl/drasyl)](https://hub.docker.com/r/drasyl/drasyl)
+[![Discord](https://img.shields.io/discord/959492172560891905)](https://discord.gg/2tcZPy7BCu)
+
+# drasyl
+
+[drasyl](https://github.com/drasyl/drasyl-rs) [dʁazy:l] is a high-performance framework for rapid development of distributed applications.
+
+drasyl enables quick and easy development of high-performance distributed applications. Driven by a secure and low-latency overlay network protocol, drasyl provides any-to-any connectivity between all Internet-enabled devices.
+
+To prevent developers from reinventing the wheel, drasyl provides extensions that address common challenges for distributed systems like identity/key management, resource discovery, and locality-aware message routing. By using drasyl, developers are unburdened of many usual tasks and can proceed directly with building their distributed application.
+
+<img src="https://docs.drasyl.org/img/drasyl-architecture.svg" alt="drasyl architecture" width="650px">
+
+# Features
+
+* Any-to-any connectivity between all Internet-enabled devices.
+* Locality-aware message routing.
+* Middlebox traversal (like firewalls or network address translators).
+* Driven by a secure & low-latency overlay network protocol.
+* Cryptographic identity & key management.
+* Rich set of extensions for perfect forward secrecy communication, service & resource discovery, reliable transport, error control, traffic shaping, etc.
+
+# Usage & Documentation
+
+* [Getting Started](https://docs.drasyl.org/language-bindings/python/)
+* [Examples](https://github.com/drasyl/drasyl/tree/master/drasyl-shared-library/examples/python)
+
+# License
+
+This is free software under the terms of the [MIT License](https://github.com/drasyl/drasyl/blob/master/LICENSE).
---
+++ shared-libraries/pypi/src/drasyl/libdrasyl.py
@@ -0,0 +1,112 @@
+#
+# Copyright (c) 2020-2022 Heiko Bornholdt and Kevin Röbert
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+# IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+# DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+# OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE
+# OR OTHER DEALINGS IN THE SOFTWARE.
+#
+from ctypes import *
+import time
+import os
+import sys
+import platform
+
+_dir = os.path.dirname(os.path.realpath(__file__))
+# get the right filename
+if platform.uname()[0] == "Windows":
+    _name = "drasyl.dll"
+elif platform.uname()[0] == "Linux":
+    _name = "libdrasyl.so"
+else:
+    _name = "libdrasyl.dylib"
+_libdrasyl = cdll.LoadLibrary(os.path.join(_dir, "libdrasyl", _name))
+
+
+#
+# utilities
+#
+
+class _dotdict(dict):
+    """dot.notation access to dictionary attributes"""
+    __getattr__ = dict.get
+    __setattr__ = dict.__setitem__
+    __delattr__ = dict.__delitem__
+
+def _wrap_event(event):
+    response = {
+        'event_code': event[0].event_code
+    }
+
+    if 10 <= event[0].event_code and event[0].event_code <= 15:
+        response['node'] = _dotdict({
+            'identity': _dotdict({
+                'proof_of_work': event[0].node[0].identity[0].proof_of_work,
+                'identity_public_key': event[0].node[0].identity[0].identity_public_key.decode('UTF-8'),
+                'identity_secret_key': event[0].node[0].identity[0].identity_secret_key.decode('UTF-8')
+            })
+        })
+    elif 20 <= event[0].event_code and event[0].event_code <= 23:
+        response['peer'] = _dotdict({
+            'address': event[0].peer[0].address.decode('UTF-8')
+        })
+    elif event[0].event_code == 30:
+        response['sender'] = event[0].message_sender.decode('UTF-8')
+        response['payload'] = event[0].message_payload.decode('UTF-8')
+
+    return _dotdict(response)
+
+#
+# drasyl API
+#
+
+DRASYL_LOG_TRACE = 300
+DRASYL_LOG_DEBUG = 500
+DRASYL_LOG_INFO = 800
+DRASYL_LOG_WARN = 900
+DRASYL_LOG_ERROR = 1000
+
+# Signals that the node has been started
+DRASYL_EVENT_NODE_UP = 10
+# Signals that the node is shut down
+DRASYL_EVENT_NODE_DOWN = 11
+# Signals that the node is currently connected to a super peer
+DRASYL_EVENT_NODE_ONLINE = 12
+# Signals that the node is currently not connected to a super peer
+DRASYL_EVENT_NODE_OFFLINE = 13
+# Signals that the node encountered an unrecoverable error
+DRASYL_EVENT_NODE_UNRECOVERABLE_ERROR = 14
+# Signals that the node has terminated normally
+DRASYL_EVENT_NODE_NORMAL_TERMINATION = 15
+# Signals that the node has established a direct connection to a peer
+DRASYL_EVENT_PEER_DIRECT = 20
+# Signals that communication with this peer is only possible by relaying messages via a super peer
+DRASYL_EVENT_PEER_RELAY = 21
+# Signals that currently all messages from and to the peer are encrypted with a long time key
+DRASYL_EVENT_LONG_TIME_ENCRYPTION = 22
+# Signals that currently all messages from and to the {@code #peer} are encrypted with an ephemeral session key
+DRASYL_EVENT_PERFECT_FORWARD_SECRECY_ENCRYPTION = 23
+# Signals that the node has received a message addressed to it
+DRASYL_EVENT_MESSAGE = 30
+# Signals that the node was unable to process an inbound message
+DRASYL_EVENT_INBOUND_EXCEPTION = 40
+
+_libdrasyl.drasyl_version.restype = c_char_p
+def drasyl_version():
+    version = _libdrasyl.drasyl_version()
+    return version.decode("utf-8").strip("\0")
+
+
