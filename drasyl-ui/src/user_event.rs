b1f6f0eba34828844c8ead24957b6b6deea85527
Heiko Bornholdt <heiko@bornholdt.io>
2025-07-09 18:53:06 +0200

    UI: Networks can now be added and removed

---
+++ drasyl-ui/src/user_event.rs
@@ -0,0 +1,27 @@
+use drasyl_sdn::rest_api::Status;
+use std::fmt::{Debug, Formatter};
+use tray_icon::TrayIconEvent;
+use tray_icon::menu::MenuEvent;
+
+#[allow(clippy::large_enum_variant)]
+pub enum UserEvent {
+    TrayIconEvent(TrayIconEvent),
+    MenuEvent(MenuEvent),
+    Status(Result<Status, String>),
+    Quit,
+    Redraw(std::time::Duration),
+    AddNetwork(String),
+}
+
+impl Debug for UserEvent {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        match self {
+            UserEvent::TrayIconEvent(_) => write!(f, "TrayIconEvent"),
+            UserEvent::MenuEvent(_) => write!(f, "MenuEvent"),
+            UserEvent::Status(_) => write!(f, "Status"),
+            UserEvent::Quit => write!(f, "Quit"),
+            UserEvent::Redraw(_) => write!(f, "Redraw"),
+            UserEvent::AddNetwork(_) => write!(f, "AddNetwork"),
+        }
+    }
+}
