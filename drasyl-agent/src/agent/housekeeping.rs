f763f4482b8c23b3e3b0ddf63d882f1bffda9f5a
Heiko Bornholdt <heiko@bornholdt.io>
2025-08-27 18:17:29 +0200

    Agent: move routing in dedicated module

--- drasyl-agent/src/agent/housekeeping.rs
+++ drasyl-agent/src/agent/housekeeping.rs
@@ -1,12 +1,10 @@
 use crate::agent::Error;
 use crate::agent::inner::AgentInner;
 use crate::agent::inner::is_drasyl_control_packet;
-use crate::network::config::EffectiveRoutingList;
 use crate::network::{LocalNodeState, Network, NetworkInner, TunState};
 use etherparse::Ipv4HeaderSlice;
 use ipnet::{IpNet, Ipv4Net};
 use ipnet_trie::IpnetTrie;
-use net_route::Handle;
 use p2p::identity::PubKey;
 use p2p::node::SendHandle;
 use p2p::util;
@@ -201,16 +199,13 @@ impl AgentInner {
             let applied_routes = match current.as_ref().map(|state| state.routes.clone()) {
                 Some(current_routes) if current_routes == desired.routes => current_routes,
                 current_routes => {
-                    Self::update_routes(
-                        self.routes_handle.clone(),
-                        current_routes,
-                        Some(desired.routes.clone()),
-                        network
-                            .tun_state
-                            .as_ref()
-                            .and_then(|tun| tun.device.if_index().ok()),
-                    )
-                    .await
+                    self.routing
+                        .update_routes(
+                            current_routes,
+                            Some(desired.routes.clone()),
+                            network.tun_state.as_ref().map(|tun| tun.device.clone()),
+                        )
+                        .await
                 }
             };
 
@@ -259,15 +254,12 @@ impl AgentInner {
             // routes
             let routes = { network.state.as_ref().map(|state| state.routes.clone()) };
             if let Some(routes) = routes {
-                Self::remove_routes(
-                    self.routes_handle.clone(),
-                    routes,
-                    network
-                        .tun_state
-                        .as_ref()
-                        .and_then(|tun| tun.device.if_index().ok()),
-                )
-                .await;
+                self.routing
+                    .remove_routes(
+                        routes,
+                        network.tun_state.as_ref().map(|tun| tun.device.clone()),
+                    )
+                    .await;
             }
 
             network.state = None;
@@ -717,75 +709,6 @@ impl AgentInner {
 
         trace!("TUN runner done.");
         Ok(())
-    }
-
-    pub(crate) async fn remove_routes(
-        routes_handle: Arc<Handle>,
-        routes: EffectiveRoutingList,
-        if_index: Option<u32>,
-    ) {
-        Self::update_routes(routes_handle, Some(routes), None, if_index).await;
-    }
-
-    async fn update_routes(
-        routes_handle: Arc<Handle>,
-        current_routes: Option<EffectiveRoutingList>,
-        desired_routes: Option<EffectiveRoutingList>,
-        if_index: Option<u32>,
-    ) -> EffectiveRoutingList {
-        let mut applied_routes = EffectiveRoutingList::default();
-        trace!(
-            "Routes change: current={:?}; desired={:?}",
-            current_routes, desired_routes
-        );
-
-        // clean up old routes
-        if let Some(current_routes) = current_routes.as_ref() {
-            for (dest, route) in current_routes.iter() {
-                match desired_routes.as_ref() {
-                    Some(desired_routes) if desired_routes.contains(dest) => {
-                        applied_routes.add(route.as_applied_route());
-                    }
-                    _ => {
-                        trace!("delete route: {:?}", route);
-                        let net_route = route.net_route(if_index);
-                        if let Err(e) = routes_handle.delete(&net_route).await {
-                            warn!("Failed to delete route {:?}: {}", route, e);
-                            applied_routes.add(route.as_removing_route());
-                        }
-                    }
-                }
-            }
-        }
-
-        if let Some(desired_routes) = desired_routes.as_ref() {
-            if let Ok(existing_routes) = routes_handle.list().await {
-                for (_, route) in desired_routes.iter() {
-                    let net_route = route.net_route(if_index);
-                    let existing = existing_routes.iter().any(|route| {
-                        net_route.destination == route.destination
-                            && net_route.prefix == route.prefix
-                            && net_route.gateway == route.gateway
-                    });
-                    if existing {
-                        trace!("route does already exist: {:?}", route);
-                        applied_routes.add(route.as_applied_route());
-                    } else {
-                        trace!("add route: {:?}", route);
-                        if let Err(e) = routes_handle.add(&net_route).await {
-                            warn!("Failed to add route {:?}: {}", route, e);
-                            applied_routes.add(route.as_pending_route());
-                        } else {
-                            applied_routes.add(route.as_applied_route());
-                        }
-                    }
-                }
-            } else {
-                warn!("Failed to list existing routes");
-            }
-        }
-
-        applied_routes
     }
 }
 
