689c17fefb75072e2574c2f195944ea3c2362f23
Heiko Bornholdt <heiko@bornholdt.io>
2025-10-08 09:34:27 +0200

    zbus

--- netsody-agent/examples/dns.rs
+++ netsody-agent/examples/dns.rs
@@ -1,4 +1,28 @@
-use zbus::Connection;
+use zbus::{proxy, Connection};
+
+#[proxy(
+    interface = "org.freedesktop.DBus.Peer",
+    default_path = "/org/freedesktop/resolve1",
+    default_service = "org.freedesktop.resolve1"
+)]
+trait Peer {
+    fn ping(&self) -> zbus::Result<()>;
+}
+
+#[proxy(
+    interface = "org.freedesktop.resolve1.Manager",
+    default_path = "/org/freedesktop/resolve1",
+    default_service = "org.freedesktop.resolve1"
+)]
+trait Manager {
+    #[zbus(property, name = "DNSStubListener")]
+    fn dns_stub_listener(&self) -> zbus::Result<String>;
+
+    fn flush_caches(&self) -> zbus::Result<()>;
+}
+
+
+// busctl get-property org.freedesktop.resolve1 /org/freedesktop/resolve1 org.freedesktop.resolve1.Manager DNSStubListener
 
 #[tokio::main]
 async fn main() {
@@ -18,7 +42,7 @@ async fn main() {
         println!("---- Parsed /etc/resolv.conf -----\n{:#?}\n", cfg);
     }
 
-    if true {
+    if false {
         // System- oder Session-Bus? resolve1 läuft im System-Bus:
         let connection = Connection::system().await.expect("Failed to connect to system bus");
 
@@ -33,6 +57,28 @@ async fn main() {
             )
             .await.expect("Failed to call method");
 
-        println!("Ping reply: {:?}", reply);
+        println!("Reply: {:?}", reply);
+    }
+
+    if false {
+        // System- oder Session-Bus? resolve1 läuft im System-Bus:
+        let connection = Connection::system().await.expect("Failed to connect to system bus");
+
+        let proxy = PeerProxy::new(&connection).await.expect("Failed to create proxy");
+        let reply = proxy.ping().await.expect("Failed to call method");
+        println!("Reply: {:?}", reply);
+    }
+
+    if true {
+        // System- oder Session-Bus? resolve1 läuft im System-Bus:
+        let connection = Connection::system().await.expect("Failed to connect to system bus");
+
+        let proxy = ManagerProxy::new(&connection).await.expect("Failed to create proxy");
+        let reply = proxy.dns_stub_listener().await.expect("Failed to call method");
+        let stub_active = reply == "yes";
+        println!("stub_active: {:?}", stub_active);
+
+        let reply = proxy.flush_caches().await.expect("Failed to call method");
+        println!("Reply: {:?}", reply);
     }
 }
